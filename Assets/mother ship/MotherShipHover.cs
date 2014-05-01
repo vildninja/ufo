718d0abfa7d29836a1f165c83f4a42ab394686f6
Jannek Egeskov Kristensen <jannek.ek@gmail.com>
2014-05-01 23:33:38 +0200

    wobbly ms + tougher humans

---
+++ Assets/mother ship/MotherShipHover.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotherShipHover : MonoBehaviour {
+
+    public Transform target;
+    public float force;
+    public float torque;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 direction = target.position - transform.position;
+        rigidbody2D.AddForce(direction * force);
+
+        float angle = target.eulerAngles.z - transform.eulerAngles.z;
+        while (angle > 180)
+            angle -= 360;
+        while (angle < -180)
+            angle += 360;
+
+        //print(transform.eulerAngles.z + " " + target.eulerAngles.z + " " + angle);
+        rigidbody2D.AddTorque(angle * torque);
+    }
+}
